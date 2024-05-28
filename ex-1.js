ebab5169bc6be385deaa7cda6cc2d89880f4e42c
Emmiiez <emmiiez.m@gmail.com>
2024-05-28 22:56:26 +0700

    Update ex-1.js
    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 // Exercise #1: Food Order
 
-// Start coding here
+const foodOrder = {};
+foodOrder.orderNumber = "A0234";
+foodOrder.address = "Bangkok"
+foodOrder.paymentType = "Cash"
+foodOrder.totalPrice = 4500
+foodOrder.restaurantName = "MK"
+
+foodOrder.totalPrice = 5000
+foodOrder.paymentType = "Credit"
+
+console.log(foodOrder);
